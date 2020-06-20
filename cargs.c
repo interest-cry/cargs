4e4e32e07d6831da531bb4776667e8a8e1fff553
yyn <y_y_n@126.com>
2020-06-20 11:39:24 +0800

    add funtion

--- cargs.c
+++ cargs.c
@@ -4,11 +4,103 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define NAME_LEN 32
+#define VALUVE_LEN 64
+#define ARG_COUNT 32
+enum { STRING, INT };
 
+typedef struct Argument {
+  int type;             // 8,0:string;1:int
+  int* dst_int;         // 8
+  char* dst_string;     // 8
+  char name[NAME_LEN];  // 32
+  char use[NAME_LEN];   // 32
+
+} Argument;
+
+typedef struct Arguments {
+  Argument arg[ARG_COUNT];
+  int len;  //参数个数
+} Arguments;
+Arguments Args;
 void print_log(const char* pre, const char* param_name) {
   printf("local:%s ,please input (%s) argument, and the value\n", pre,
          param_name);
 }
+void print_value(const char* pre, Argument a) {
+  printf("%stype:%d,name:%s,", pre, a.type, a.name);
+  if (a.dst_string != NULL) {
+    printf("dst_string:%s,", a.dst_string);
+  }
+
+  if (a.dst_int != NULL) {
+    printf("dst_int:%d,", *(a.dst_int));
+  }
+  printf("\n");
+}
+void print_args(const char* prefix, const Arguments* args) {
+  assert(args != NULL);
+  printf("%s\nargs->len:%d\n", prefix, args->len);
+  for (int i = 0; i < args->len; i++) {
+    printf("index:%d\n", i);
+    print_value(NULL, args->arg[i]);
+  }
+}
+void args_init() {
+  printf(
+      "sizeof(Arguments)=%ld,sizeof(Argument)=%ld,sizeof(int)=%ld,sizeof(int*)="
+      "%ld\n",
+      sizeof(Arguments), sizeof(Argument), sizeof(int), sizeof(int*));
+  memset(&Args, 0, sizeof(Arguments));
+}
+void args_set_string(char* dst_string, const char* name, const char* value,
+                     const char* use) {
+  assert(dst_string != NULL);
+  assert(name != NULL);
+  // memcpy(args->arg + args->len, &a, 1);
+  int index = Args.len;
+  Args.arg[index].type = STRING;
+  Args.arg[index].dst_string = dst_string;
+  strcpy(Args.arg[index].name, name);
+  if (value != NULL) {
+    strcpy(Args.arg[index].dst_string, value);
+  } else {
+    strcpy(Args.arg[index].dst_string, "");
+  }
+
+  if (use != NULL) {
+    strcpy(Args.arg[index].use, use);
+  } else {
+    strcpy(Args.arg[index].use, "");
+  }
+
+  Args.len++;
+}
+
+void args_set_int(int* dst_int, const char* name, int value, const char* use) {
+  assert(dst_int != NULL);
+  assert(name != NULL);
+  int index = Args.len;
+  Args.arg[index].type = INT;
+  Args.arg[index].dst_int = dst_int;
+  strcpy(Args.arg[index].name, name);
+  *(Args.arg[index].dst_int) = value;
+  if (use != NULL) {
+    strcpy(Args.arg[index].use, use);
+  } else {
+    strcpy(Args.arg[index].use, "");
+  }
+  Args.len++;
+}
+
+void args_use() {
+  printf("-h, --help :help infomation\n");
+  for (int i = 0; i < Args.len; i++) {
+    printf("-%-32s,--%-32s (%s)\n", Args.arg[i].name, Args.arg[i].name,
+           Args.arg[i].use);
+  }
+}
+
 int parse_one_arg(int argc, char* argv[], Argument* arg) {
   for (int i = 1; i < argc;) {
     // args->arg[i].
@@ -42,77 +134,74 @@ int parse_one_arg(int argc, char* argv[], Argument* arg) {
       } else {
         strcpy(name, name_tmp);
       }
-
     } else {
       strcpy(name, name_tmp + 2);
     }
     // name not equal
     if (strcmp(name, arg->name)) {
-      //   if (status) {  // has '='
-      //     i += 1;
-      //   } else {  // no "="
-      //     i += 2;
-      //   }
       i += 1;
       if (i >= argc) {
         // todo:打印日志
         printf("===========>>>>>>>i:%d\n", i);
         print_log("22", arg->name);
-        return -1;
+        return 0;
       }
       continue;
     }
     // name equal
-    if (strcmp(arg->type, "int")) {
-      strcpy(arg->value_char, value);
+    if (arg->type == STRING) {
+      strcpy(arg->dst_string, value);
       return 0;
-
     } else {  // type int
-      arg->value_int = atoi(value);
+      *arg->dst_int = atoi(value);
       return 0;
     }
   }
 }
-int parse_args(int argc, char* argv[], Arguments* args) {
+
+void args_parse(int argc, char* argv[]) {
+  assert(Args.len > 0);
+  int eq1 = strcmp(argv[argc - 1], "-h");
+  int eq2 = strcmp(argv[argc - 1], "--help");
+  if (argc == 1 || eq1 == 0 || eq2 == 0) {
+    args_use();
+    exit(0);
+  }
   int flag = 0;
-  for (int i = 1; i < args->num; i++) {
-    flag = parse_one_arg(argc, argv, &args->arg[i]);
-    printf("#############i:%d\n", i);
+  for (int i = 0; i < Args.len; i++) {
+    flag = parse_one_arg(argc, argv, &Args.arg[i]);
     if (flag) {
-      printf("================\n");
-      return flag;
-    }
-  }
-  return 0;
-}
-void print_args(const char* prefix, Arguments args) {
-  printf("********%s********\n", prefix);
-  for (int i = 1; i < args.num; i++) {
-    printf("\nindex:%d\n", i);
-    printf("===type:%s, %ld\n", args.arg[i].type, strlen(args.arg[i].type));
-    printf("===name:%s\n", args.arg[i].name);
-    if (!strcmp(args.arg[i].type, "char")) {
-      printf("===value_char:%s\n", args.arg[i].value_char);
-    } else {
-      printf("===value_int>>:%d\n", args.arg[i].value_int);
+      args_use();
+      exit(0);
     }
   }
 }
+
 #if 1
 int main(int argc, char* argv[]) {
-  Arguments args = {{{"", "", "", 0},
-                     {"char", "fn", "", 0},
-                     {"char", "tm", "", 0},
-                     {"char", "nt", "", 0},
-                     {"char", "num", "", 0},
-                     {"char", "level", "", 0}},
-                    6};
-  print_args("before parse", args);
-  int fg = parse_args(argc, argv, &args);
-  if (fg) {
-    printf("parse argument error...\n");
-    return fg;
-  }
-  print_args("after parse", args);
+  Arguments aa;
+  char fname[32] = {0}, time[32] = {0};
+  int num = 0, nt = 0;
+  args_init();
+  args_set_string(fname, "fn", "1111", "file name");
+  args_set_string(time, "tm", "11119999", "tmie name");
+  args_set_int(&num, "num", 77777, "number");
+  args_set_int(&nt, "nt", 1, "thread number");
+  print_args(NULL, &Args);
+  args_parse(argc, argv);
+  // assert(fg == 0);
+  // if (fg) {
+  //   return 0;
+  //   /* code */
+  // }
+
+  printf("fname:%s,time:%s,num:%d,nt:%d\n", fname, time, num, nt);
+  // print_args("before parse", args);
+  // int fg = parse_args(argc, argv, &args);
+  // if (fg) {
+  //   printf("parse argument error...\n");
+  //   return fg;
+  // }
+  // print_args("after parse", args);
 }
 #endif
--- cargs.h
+++ cargs.h
@@ -4,24 +4,10 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-#define NAME_LEN 32
-#define VALUVE_LEN 64
-#define ARG_COUNT 32
-typedef struct Argument {
-  char type[NAME_LEN];
-  char name[NAME_LEN];
-  char value_char[VALUVE_LEN];
-  int value_int;
 
-} Argument;
-
-typedef struct Arguments {
-  Argument arg[ARG_COUNT];
-  int num;  //参数个数
-  // int (*parse)(const char*, Argument*);
-} Arguments;
-
-int parse_args(int argc, char* argv[], Arguments* args);
+// void init(Arguments* args);
+// void set_argument(Arguments* args, Argument a);
+// int parse_args(int argc, char* argv[], Arguments* args);
 #ifdef __cplusplus
 }
 #endif
