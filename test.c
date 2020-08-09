c0c7bdfb320054a9f7e0a60d4180e3693a2be04e
yyn <y_y_n@126.com>
2020-08-09 08:38:59 +0800

    modify to utf-8 LF

--- test.c
+++ test.c
@@ -5,15 +5,16 @@
 #include "cargs.h"
 #if 1
 int main(int argc, char* argv[]) {
-  char fname[32] = {0}, time[32] = {0};
-  int num = 0, nt = 0;
-  args_init();
-  args_set_string(fname, "fn", "1111", "file name");
-  args_set_string(time, "tm", "11119999", "tmie name");
-  args_set_int(&num, "num", 77777, "number");
-  args_set_int(&nt, "nt", 1, "thread number");
-  // print_args(NULL, &Args);
-  args_parse(argc, argv);
-  printf("fname:%s,time:%s,num:%d,nt:%d\n", fname, time, num, nt);
+    char fname[32] = {0}, time[32] = {0};
+    int num = 0, nt = 0;
+    args_init();
+    args_set_string(fname, "fn", "1111", "file name");
+    args_set_string(time, "tm", "11119999", "tmie name");
+    args_set_int(&num, "num", 77777, "number");
+    args_set_int(&nt, "nt", 1, "thread number");
+    // print_args(NULL, &Args);
+
+    args_parse(argc, argv);
+    printf("fname:%s,time:%s,num:%d,nt:%d\n", fname, time, num, nt);
 }
 #endif
